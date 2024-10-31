6514f5c7aa9d1fe33c2bf33f1a5d69dbcf90a978
Vitu <vitorcsharp@hotmail.com>
2024-10-31 16:32:49 -0300

    criando component e testando variavel com interpolação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ComponentFilhoComponent } from './components/component-filho/component-filho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ComponentFilhoComponent
   ],
   imports: [
     BrowserModule,
