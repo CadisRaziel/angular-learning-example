93bfbcf8921f6f95104dc1734c052149f039eca4
Vitu <vitorcsharp@hotmail.com>
2024-11-01 11:53:32 -0300

    usando interfaces(models)

--- src/app/components/lists-for/lists-for.component.ts
+++ src/app/components/lists-for/lists-for.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/interfaces-models/animals';
 
 @Component({
   selector: 'app-lists-for',
@@ -14,6 +15,21 @@ export class ListsForComponent implements OnInit {
     {name: "Bob", type: "Horse"},
   ];
 
+  //Aqui usaremos a interface(model)
+  animalsComInterface: Animal[] = [ 
+    {name: "Turca", type: "Dog", age: 4},
+    {name: "Tom", type: "Cat", age: 10},
+    {name: "Frida", type: "Dog", age: 1},
+    {name: "Bob", type: "Horse", age: 2},
+  ];
+  
+  //Aqui usaremos a interface(model)
+  animalComInterface: Animal = {
+    name: "Nameteste",
+    type: "Typetest",
+    age: 10
+  }
+
   peooples = [ 
     "Juca", "Pedrin", "Joaozin"
   ];
@@ -25,9 +41,16 @@ export class ListsForComponent implements OnInit {
   //transformar o objeto carros em um array de pares [chave, valor], para poder ser feita iteração em um ngFor
   carrosArray = Object.entries(this.carros);
 
+
+  animalDetailsComInterface = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showAge(animalInterface: Animal) {
+    this.animalDetailsComInterface = `O animal ${animalInterface.name} tem ${animalInterface.age}`;
   }
 
 }
---
+++ src/app/interfaces-models/animals.ts
@@ -0,0 +1,6 @@
+//!no angular o model costuma ser interface
+export interface Animal {
+    name: string;
+    type: string;
+    age: number;
+}
